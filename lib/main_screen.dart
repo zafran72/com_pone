bb0759a7d9d6fcc01365ec6efca8b6c10aa7af8e
zafran72 <tzafran72@gmail.com>
2022-08-23 00:45:35 +0700

    Update main_screen.dart

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -12,75 +12,152 @@ class MainScreen extends StatelessWidget {
           title: const Text("Com-Pone"),
           backgroundColor: const Color(0xFF000000),
         ),
-        body: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(20),
-          childAspectRatio: (140 / 190),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 2,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.black,
-              ),
-              child: Image.asset(
-                "images/cozmo.png",
-              ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(top: 10, left: 20),
+                  child: const Text(
+                    "Hi Zafran",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10, left: 20),
+                  child: const Text(
+                    "Choose and discover the PC’s component",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+                GridView.builder(
+                    primary: false,
+                    padding: const EdgeInsets.all(10),
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      childAspectRatio: 140 / 190,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemCount: 6,
+                    itemBuilder: (BuildContext ctx, index) {
+                      return Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Image.asset(
+                              "images/cozmo.png",
+                              height: 210,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 12),
+                              child: Text(
+                                "COZMO",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+                // GridView.count(
+                //   primary: false,
+                //   padding:
+                //       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                //   childAspectRatio: (140 / 190),
+                //   crossAxisSpacing: 10,
+                //   mainAxisSpacing: 10,
+                //   crossAxisCount: 2,
+                //   shrinkWrap: true,
+                //   children: <Widget>[
+                //     Container(
+                //         padding: const EdgeInsets.all(15),
+                //         decoration: BoxDecoration(
+                //           borderRadius: BorderRadius.circular(15),
+                //           color: Colors.black,
+                //         ),
+                // child: Column(
+                //   children: <Widget>[
+                //     Image.asset(
+                //       "images/cozmo.png",
+                //     ),
+                //     // Text("Cozmo")
+                //   ],
+                // )),
+                //     Container(
+                //       padding: const EdgeInsets.all(15),
+                //       decoration: BoxDecoration(
+                //         borderRadius: BorderRadius.circular(15),
+                //         color: Colors.black,
+                //       ),
+                //       child: Image.asset(
+                //         "images/cozmo.png",
+                //       ),
+                //     ),
+                //     Container(
+                //       padding: const EdgeInsets.all(15),
+                //       decoration: BoxDecoration(
+                //         borderRadius: BorderRadius.circular(15),
+                //         color: Colors.black,
+                //       ),
+                //       child: Image.asset(
+                //         "images/cozmo.png",
+                //       ),
+                //     ),
+                //     Container(
+                //       padding: const EdgeInsets.all(15),
+                //       decoration: BoxDecoration(
+                //         borderRadius: BorderRadius.circular(15),
+                //         color: Colors.black,
+                //       ),
+                //       child: Image.asset(
+                //         "images/cozmo.png",
+                //       ),
+                //     ),
+                //     Container(
+                //       padding: const EdgeInsets.all(15),
+                //       decoration: BoxDecoration(
+                //         borderRadius: BorderRadius.circular(15),
+                //         color: Colors.black,
+                //       ),
+                //       child: Image.asset(
+                //         "images/cozmo.png",
+                //       ),
+                //     ),
+                //     Container(
+                //       padding: const EdgeInsets.all(15),
+                //       decoration: BoxDecoration(
+                //         borderRadius: BorderRadius.circular(15),
+                //         color: Colors.black,
+                //       ),
+                //       child: Image.asset(
+                //         "images/cozmo.png",
+                //       ),
+                //     ),
+                //   ],
+                // )
+              ],
             ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.black,
-              ),
-              child: Image.asset(
-                "images/cozmo.png",
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.black,
-              ),
-              child: Image.asset(
-                "images/cozmo.png",
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.black,
-              ),
-              child: Image.asset(
-                "images/cozmo.png",
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.black,
-              ),
-              child: Image.asset(
-                "images/cozmo.png",
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.black,
-              ),
-              child: Image.asset(
-                "images/cozmo.png",
-              ),
-            ),
-          ],
+          ),
         ));
   }
 }
